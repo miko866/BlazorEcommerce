c6a936e1ee9305064b2425787d59a22253f3ed08
miko866 <mdurik2@gmail.com>
2023-01-12 21:37:16 +0100

    first commit

---
+++ BlazorEcommerce/Shared/Product.cs
@@ -0,0 +1,13 @@
+namespace BlazorEcommerce.Shared;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+}
