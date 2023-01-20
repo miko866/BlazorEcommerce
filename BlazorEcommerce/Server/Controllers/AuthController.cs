1dc2ddf6a1792b751a6d0fcb490724069bb229a2
miko866 <mdurik2@gmail.com>
2023-01-20 20:15:19 +0100

    Adds register user

---
+++ BlazorEcommerce/Server/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
+        {
+            var response = await _authService.Register(new User
+            {
+                Email = request.Email
+            }, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -0,0 +1,49 @@
+namespace BlazorEcommerce.Server.Services.AuthService;
+
+using System.Security.Cryptography;
+
+public class AuthService : IAuthService
+{
+    private readonly DataContext _dataContext;
+
+    public AuthService(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+    
+    public async Task<ServiceResponse<int>> Register(User user, string password)
+    {
+        if (await UserExists(user.Email))
+        {
+            return new ServiceResponse<int> { Success = false, Message = "user already exists." }; 
+        }
+        
+        CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        _dataContext.Users.Add(user);
+        await _dataContext.SaveChangesAsync();
+
+        return new ServiceResponse<int> { Data = user.Id };
+    }
+
+    public async Task<bool> UserExists(string email)
+    {
+        if (await _dataContext.Users.AnyAsync(user => user.Email.ToLower().Equals(email.ToLower())))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
